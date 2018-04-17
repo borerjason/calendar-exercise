32e06c085244b49cd63ced88150d5bf84a42fc09
Jason Borer <borerjason@gmail.com>
2018-04-16 20:12:29 -0700

    update proptypes

--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -1,4 +1,5 @@
-import React, {PureComponent, PropTypes} from 'react';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 import {EVENTS_PROP_TYPE} from './constants';
 import {getDisplayHour} from '../utils';
 import TimeSlotEvent from './TimeSlotEvent';
